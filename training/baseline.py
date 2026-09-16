7a7732bd79450cd52df6616d6a385325758e6ae5
Mingze Zhang <zhangmingze1016@gmail.com>
2026-09-16 14:10:10 -0400

    baseline 1st ediition

---
+++ training/baseline.py
@@ -0,0 +1,83 @@
+import pandas as pd
+from catboost import CatBoostClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# Load the training data
+train = pd.read_csv("data/train.csv")   
+test = pd.read_csv("data/test.csv")
+
+# Separate features and target variable
+
+TARGET = "Will_Buy_EV"
+
+X = train.drop(columns=[TARGET, "id"])
+y = train[TARGET]
+
+X_test = train.drop(columns= ["id"])
+
+#find categorical columns
+
+cat_cols = X.select_dtypes(include=["object", "str"]).columns.tolist()
+
+print("catgorial columns:")
+print(cat_cols)
+
+#train
+
+X_train, X_val, y_train,y_val = train_test_split(
+    X,
+    y,
+    test_size = 0.2,
+    random_state = 42,
+    stratify = y,
+)
+
+#model
+
+model = CatBoostClassifier(
+    iterations = 5000,
+    depth = 6,
+    learning_rate = 0.05,
+    random_seed = 42,
+    verbose = 100
+)
+
+#train
+
+model.fit(
+    X_train,
+    y_train,
+    cat_features= cat_cols
+        )
+
+#validate
+
+val_pred = model.predict(X_val).ravel()
+
+accuracy = accuracy_score(y_val, val_pred)
+
+print(f"Validation accuracy: {accuracy:.5f}")
+
+final_model = CatBoostClassifier(
+    iterations=500,
+    depth=6,
+    learning_rate=0.05,
+    random_seed=42,
+    verbose=100
+)
+
+final_model.fit(
+    X,
+    y,
+    cat_features=cat_cols
+)
+
+test_pred = final_model.predict(X_test).ravel()
+
+
+
+
+
+
+
